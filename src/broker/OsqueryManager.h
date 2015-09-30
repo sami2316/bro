b03d79dd4ef901b8fad400e7f5319e7b5c54b406
sami2316 <sami2316@users.noreply.github.com>
2015-09-30 15:00:39 +0500

    Update OsqueryManager.h

--- src/broker/OsqueryManager.h
+++ src/broker/OsqueryManager.h
@@ -17,15 +17,16 @@ namespace bro_broker {
 
 	/*
 	 * QueryManager class is responsible for point to point peer connection
-	 * (single master to single host), group of peers connection (signle master
-	 * to multiple hosts), single query subscription at osquery side and multiple
-	 * queries subscription at osquery side. 
+	 * (single master to a single host) and point to multipoint connection 
+         * (single master
+	 * to multiple hosts). The class also enables a master to subscribe 
+         * either to a single query or to multiple queries at the monitoring 
+         * hosts. 
+	 * This class publically inherits Manager class and adds additoinal 
+         * functionality
 	 * 
-	 * This class publically inherits Manager class and adds additoinal functionality
-	 * (group of connections single query subscription and group of queries of 
-	 * subscription).
-	 * 
-	 * To establish sigle host connection, this class provides base class Connect().
+	 * To establish point to point peer connection, this class also uses 
+         * the base class Connect().
 	 */
 
 class OsqueryManager : public Manager {
@@ -43,55 +44,69 @@ public:
 
 	/**
 	 * Initiate a remote connection to all hosts of a subgroup.
+	 *
 	 * @param addr_port A bro table (string of string) which contains
-	 * IP address as key and port as value.
-	 * 
-	 * @param retry_interval an interval at which to retry establishing the
-	 * connection with the remote peer.
-	 * @return true if it's possible to try connecting with the peers and
-	 * they're new peers.  
+	 * the IP address as a key and the port as a value.
+	 * @param retry_interval an interval at which the master host retries 
+         * establishing a broken connection with the remote hosts of a group.
+	 *
+	 * @return true if the connections are successfully established.  
 	 */
 	bool GroupConnect(RecordVal* addr_port,
 	             std::chrono::duration<double> retry_interval);
+
 	/**
-	 * Send a subscription message as event to any interested peers
-	 * (single query subscription to remote host/hosts of same interest).
-	 * @param topic a topic string associated with the print message.
-	 * Peers advertise interest by registering a subscription to some prefix
-	 * of this topic name.
-	 * @param args the event and SQL query as EventArgs record value to send to peers.
+	 * Send a subscription message as an event to interested peers
+	 * (a single query subscription message to remote host or hosts that 
+         * have the interest in the same broker topic).
+	 *
+	 * @param topic A topic string associated with the print message.
+	 * Peers advertise interest by registering to a subscription query 
+         * related to a topic name.
+	 * @param args the event and SQL query as "EventArgs record values" sent
+         *  to a host or group of hosts.
+	 * @param inidump set it to true if Master is interested in initial 
+         * dump.
 	 * @param flags tune the behavior of how the message is sent.
 	 *
 	 * @return true if the message is sent successfully.
 	 */	
-	bool Event(std::string topic, RecordVal* args, int flags);
+	bool Event(std::string topic, RecordVal* args,  bool inidump, 
+        Val* flags);
 
 	/**
-	 * Send multiple subscripiton messages(multiple queries) as events to any interested peers.
-	 * (multiple queries subscription to remote host/hosts).
+	 * Send multiple subscripiton messages (multiple queries) as events to 
+         * interested peers.
+	 * (multiple queries subscription to remote host or hosts).
+	 *
 	 * @param topic A topic string associated with the print message.
-	 * Peers advertise interest by registering a subscription to some prefix
-	 * of this topic name.
-	 * @param args group of events and SQL query pairs to send to peers.  
+	 * Peers advertise interest by registering to a subscription query 
+         * related to a topic name.
+	 * @param args a group of events and corresponding SQL queries are sent
+         *  to a host or group of hosts.  
+	 * @param inidump set it to true if Master is interested in initial 
+         * dump.
 	 * @param flags tune the behavior of how the message is send.
 	 * 
 	 * @return true if the messages are sent successfully.
 	 */	
-	bool GroupEvent(std::string topic, RecordVal* args, Val* flags);
+	bool GroupEvent(std::string topic, RecordVal* args, bool inidump, 
+        Val* flags);
 
 	/**
-	 * Create an EventArgs record value for single query subscription.
-	 * @param args the event and SQL query.  The event is always the first
-	 * elements in the list.
+	 * Creates an EventArgs record value for subscription to a single query.
+	 * @param args the event and SQL query. The event is always the first
+	 * element in the list.
 	 *
 	 * @return an EventArgs record value.
 	 */
 	RecordVal* MakeSubscriptionArgs(val_list* args);
 
 	/**
-	 * Create an EventArgs record value for group of queries subscription.
-	 * @param args Bro table (string of string).  The event is always the first
-	 * elements in the table row.
+	 * Creates an EventArgs record value for a subscription to multiple 
+         * queries.
+	 * @param args Bro table (string of string). The event is always the 
+         * first element in the row of bro table.
 	 *
 	 * @return an EventArgs record value.
 	 */
